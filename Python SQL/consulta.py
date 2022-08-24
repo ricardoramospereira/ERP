0a22a13cf4bd85efde95a3f93eb3714236faa8de
ricardoramospereira <ricardoramospereira33@gmail.com>
2022-08-24 12:10:05 -0300

    alteracao

--- Python SQL/consulta.py
+++ Python SQL/consulta.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
 mydb =mysql.connector.connect(
-    host = 'localhost',
-    user = 'vitor',
-    password = '2802',
-    database = 'python'
+    host = '',
+    user = '',
+    password = '',
+    database = ''
 )
 
 mycursor = mydb.cursor()
